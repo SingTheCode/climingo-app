31318573b024cfd90cdaedfb73a87dba0d296b7a
spiderq2697 <spiderq10@gmail.com>
2024-02-07 21:20:37 +0900

    Initial Commit

---
+++ 테스트 앱/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  테스트 앱
+//
+//  Created by singco on 2024/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
