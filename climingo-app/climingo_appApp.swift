5ebb9e3a5800d69f4d3ce9a66db3656f13c2c067
박재현(Singco) <singco@emart.com>
2024-07-16 10:32:16 +0900

    Initial Commit

---
+++ climingo-app/climingo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  climingo_appApp.swift
+//  climingo-app
+//
+//  Created by emart on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct climingo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
